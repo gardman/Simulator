cb807e23117d61c73c7c1fb7725d11ffcdb5ca91
Niclas Jonasson <niclasjonasson@gmail.com>
2020-05-12 12:28:45 +0200

    Added gps and created sample scene

---
+++ Assets/Scripts/Sensors/GPS/GPS.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour
+{
+    private void Start()
+    {
+        GPSEncoder.SetLocalOrigin(new Vector2(59.908110f, 17.739856f));
+    }
+
+    private void Update()
+    {
+        Debug.Log(GPSEncoder.USCToGPS(transform.position).ToString("F6"));
+    }
+}
