b28266bdc8960c0291e0ca5991c4447ce4864df8
Marcus Gårdman <marcus.gardman@gmail.com>
2020-05-18 21:54:53 +0200

    Kista gps coordinate alignment

---
+++ Assets/Scripts/FollowCar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCar : MonoBehaviour
+{
+    public Transform carT;
+    private Vector3 offset = new Vector3(0,1,5);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = (carT.transform.position + (carT.forward * -10) + Vector3.up * 3) - this.transform.position;
+        this.transform.position += diff * 1.95f * Time.deltaTime;
+
+        this.transform.LookAt(carT, Vector3.up);
+    }
+}
--- Assets/Scripts/Sensors/GPS/GPS.cs
+++ Assets/Scripts/Sensors/GPS/GPS.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GPS : MonoBehaviour
 {
+    public Vector2 initCoords = new Vector2(59.4050f, 17.9520f);
     private void Start()
     {
-        GPSEncoder.SetLocalOrigin(new Vector2(59.908110f, 17.739856f));
+        GPSEncoder.SetLocalOrigin(initCoords); //new Vector2(59.4050f, 17.9520f)); //new Vector2(59.908110f, 17.739856f));
+
+
+        Debug.Log("A:" + GPSEncoder.GPSToUCS(59.4050f, 17.9520f));
+        Debug.Log("B:" + GPSEncoder.GPSToUCS(59.4039f, 17.9488f));
+        Debug.Log("C:" + GPSEncoder.GPSToUCS(59.4030f, 17.9546f));
+
     }
 
     private void Update()
     {
-        Debug.Log(GPSEncoder.USCToGPS(transform.position).ToString("F6"));
+       // Debug.Log(GPSEncoder.USCToGPS(transform.position).ToString("F6"));
     }
 }
